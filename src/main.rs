05b889d7b11671bd7cf46f827acb873a53a4041f
Jake Scaife <jakescaife@pm.me>
2021-07-06 22:43:50 +0100

    Remove image struct

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
-struct Image {
-    width: usize,
-    height: usize,
-}
-
 fn main() {
-    let image = Image { width: 256, height: 256 };
-    let mut pixels = format!("P3\n{} {}\n255\n", image.width, image.height);
+    let width = 256;
+    let height = 256;
 
-    for row in (0..image.height - 1).rev() {
-        for col in 0..image.width {
-            let red   = col as f64 / (image.width  as f64 - 1.0);
-            let green = row as f64 / (image.height as f64 - 1.0);
+    let mut image = format!("P3\n{} {}\n255\n", width, height);
+
+    for row in (0..height - 1).rev() {
+        for col in 0..width {
+            let red   = col as f64 / (width  as f64 - 1.0);
+            let green = row as f64 / (height as f64 - 1.0);
             let blue  = 0.25;
 
             let convert = |amount| (amount * 255.999) as u8;
@@ -21,9 +18,9 @@ fn main() {
                 convert(blue)
             );
 
-            pixels.push_str(&pixel);
+            image.push_str(&pixel);
         }
     }
 
-    std::fs::write("image.ppm", pixels).unwrap();
+    std::fs::write("image.ppm", image).unwrap();
 }
