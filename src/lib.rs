418b1de150a72b686678fa28f1fcc18b2a0dc802
Jake Scaife <jakescaife@pm.me>
2021-07-06 22:40:28 +0100

    Rename vector class

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,43 @@
+use std::ops;
+
 #[derive(Copy, Clone, Debug, PartialEq)]
-pub struct Vec3 {
+pub struct Vector {
     x: f64,
     y: f64,
     z: f64,
 }
 
-impl Vec3 {
+impl Vector {
     pub fn new(x: f64, y: f64, z: f64) -> Self {
         Self { x, y, z }
     }
 }
 
-impl std::ops::Add for Vec3 {
+impl ops::Add for Vector {
     type Output = Self;
-    fn add(self, other: Self) -> Self {
-        Self::new(self.x + other.x, self.y + other.y, self.z + other.z)
+    fn add(self, v: Self) -> Self {
+        Self::new(self.x + v.x, self.y + v.y, self.z + v.z)
     }
 }
 
-impl std::ops::Sub for Vec3 {
+impl ops::Sub for Vector {
     type Output = Self;
-    fn sub(self, other: Self) -> Self {
-        Self::new(self.x - other.x, self.y - other.y, self.z - other.z)
+    fn sub(self, v: Self) -> Self {
+        Self::new(self.x - v.x, self.y - v.y, self.z - v.z)
     }
 }
 
-impl std::ops::Mul<f64> for Vec3 {
+impl ops::Mul<f64> for Vector {
     type Output = Self;
-    fn mul(self, magnitude: f64) -> Self {
-        Self::new(self.x * magnitude, self.y * magnitude, self.z * magnitude)
+    fn mul(self, x: f64) -> Self {
+        Self::new(x * self.x, x * self.y, x * self.z)
     }
 }
 
-impl std::ops::Mul<Vec3> for f64 {
-    type Output = Vec3;
-    fn mul(self, vector: Vec3) -> Vec3 {
-        vector * self
+impl ops::Mul<Vector> for f64 {
+    type Output = Vector;
+    fn mul(self, v: Vector) -> Vector {
+        v * self
     }
 }
 
@@ -45,24 +47,24 @@ mod tests {
 
     #[test]
     fn addition() {
-        let a = Vec3::new(1.0, 2.0, 3.0);
-        let b = Vec3::new(0.1, 0.2, 0.3);
-        let c = Vec3::new(1.1, 2.2, 3.3);
+        let a = Vector::new(1.0, 2.0, 3.0);
+        let b = Vector::new(0.1, 0.2, 0.3);
+        let c = Vector::new(1.1, 2.2, 3.3);
         assert_eq!(a + b, c);
     }
 
     #[test]
     fn subtraction() {
-        let a = Vec3::new(1.0, 2.0, 3.0);
-        let b = Vec3::new(0.1, 0.2, 0.3);
-        let c = Vec3::new(0.9, 1.8, 2.7);
+        let a = Vector::new(1.0, 2.0, 3.0);
+        let b = Vector::new(0.1, 0.2, 0.3);
+        let c = Vector::new(0.9, 1.8, 2.7);
         assert_eq!(a - b, c);
     }
 
     #[test]
     fn scalar_multiplication() {
-        let a = Vec3::new(1.0, 2.0, 3.0);
-        let b = Vec3::new(3.0, 6.0, 9.0);
+        let a = Vector::new(1.0, 2.0, 3.0);
+        let b = Vector::new(3.0, 6.0, 9.0);
         assert_eq!(a * 3.0, b);
         assert_eq!(3.0 * a, b);
     }
